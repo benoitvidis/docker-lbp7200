11fc69429befe180e0afa0fba553ce7c3f23d353
Benoît Vidis <contact@benoitvidis.com>
2017-03-05 12:30:34 +0100

    adding dirty sleeps

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 /usr/sbin/cupsd
-/etc/init.d/ccpd restart
 
-sleep 3 && /usr/sbin/lpadmin -p LBP7200 -P /usr/share/cups/model/CNCUPSLBP7200CCAPTK.ppd -v ccp://localhost:59687 -E
+sleep 3 
+/usr/sbin/lpadmin -p LBP7200 -P /usr/share/cups/model/CNCUPSLBP7200CCAPTK.ppd -v ccp://localhost:59687 -E
+
+sleep 2
+/etc/init.d/ccpd restart
 
 exec watch "/etc/init.d/ccpd status"
