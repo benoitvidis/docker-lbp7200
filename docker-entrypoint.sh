b1fa8ff0c66922081f04671cc902e879d3316047
Benoît Vidis <contact@benoitvidis.com>
2017-03-05 08:23:34 +0100

    fix printer name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 service cups start
 /etc/init.d/ccpd restart
 
-/usr/sbin/lpadmin -p CANON-LBP7200 -P /usr/share/cups/model/CNCUPSLBP7200CCAPTK.ppd -v ccp://localhost:59787 -E
+/usr/sbin/lpadmin -p LBP7200 -P /usr/share/cups/model/CNCUPSLBP7200CCAPTK.ppd -v ccp://localhost:59787 -E
 
 exec watch "/etc/init.d/ccpd status"
