90748a4fc237abe4104ff667ed6afee8dd0cd4d3
Benoît Vidis <contact@benoitvidis.com>
2017-03-05 09:14:47 +0100

    use ccpd 59687 port

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 service cups start
 /etc/init.d/ccpd restart
 
-sleep 3 && /usr/sbin/lpadmin -p LBP7200 -P /usr/share/cups/model/CNCUPSLBP7200CCAPTK.ppd -v ccp://localhost:59787 -E
+sleep 3 && /usr/sbin/lpadmin -p LBP7200 -P /usr/share/cups/model/CNCUPSLBP7200CCAPTK.ppd -v ccp://localhost:59687 -E
 
 exec watch "/etc/init.d/ccpd status"
